e0efdc0d8df676b5063aef296f828afd17b26d0d
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2025-02-03 14:37:32 +0100

    fix cmp with lsp

--- .config/nvim/after/plugin/cmp.lua
+++ .config/nvim/after/plugin/cmp.lua
@@ -1,17 +1,22 @@
 local cmp = require('cmp')
 
 cmp.setup({
+    mapping = cmp.mapping.preset.insert({
+        -- confirm completion
+        ['<C-e>'] = cmp.mapping.confirm({select = true}),
+    }),
     snippet = {
         expand = function(args)
             require('luasnip').lsp_expand(args.body)
         end,
     },
     sources = {
-        { name = 'nvim-lsp' },
+        { name = 'nvim_lsp' },
         { name = 'luasnip' },
         { name = 'path' },
     }
 })
+
 
 cmp.setup.filetype({ 'sql' }, {
     sources = {
