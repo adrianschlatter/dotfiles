b41a429b87805f2a9bdf2ceaec5c07922542803a
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2025-05-18 22:51:05 +0200

    nvim: copilot completion <C-L> instead of <tab>

--- .config/nvim/lua/custom/remap.lua
+++ .config/nvim/lua/custom/remap.lua
@@ -73,3 +73,8 @@ vim.keymap.set('n', '<leader>q`', 'ciw`<C-r>"`<esc>',
                { desc = 'backtick-quote word' })
 vim.keymap.set('n', '<leader>q"', 'ciw"<C-r>""<esc>',
                { desc = 'double-quote word' })
+
+-- Github copilot:
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap("i", "<C-L>", 'copilot#Accept("<CR>")',
+                        { silent = true, expr = true })
