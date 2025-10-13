d1d66696230e958487edb71c1ce0d1799b7628f9
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2025-10-13 13:40:38 +0200

    nvim: fix lsp config for esbonio (.rst)

--- .config/nvim/lua/custom/lsp.lua
+++ .config/nvim/lua/custom/lsp.lua
@@ -17,7 +17,16 @@ require('mason').setup({})
 
 -- Next, we need to configure the LSP servers. However, I already did this by
 -- installing the plugin nvim-lspconfig (see `packer.lua`, which automatically
--- configures many LSP servers => don't need to do anything here.
+-- configures many LSP servers. Here, I only correct what nvim-lspconfig got
+-- wrong.
+-- For esbonio, nvim-lspconfig uses the python environment currently active.
+-- Apparently, this is useful when working on a project using sphinx. That's
+-- not my usual use case, therefore use the python environment mason has
+-- installed esbonio into:
+vim.lsp.config('esbonio', {
+    cmd = { os.getenv('HOME') .. '/.local/share/nvim/mason/packages/esbonio/venv/bin/python',
+            '-m', 'esbonio' }
+})
 
 -- Tell vim.lsp to enable these LSP servers (not *all* that have been
 -- configured by nvim-lspconfig):
