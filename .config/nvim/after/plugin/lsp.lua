39bcacd0259ef2e4bd5bee7f6093c8e93aebf2e4
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2025-10-16 11:12:55 +0200

    nvim: don't repeat keymap help texts for every lsp server

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -4,7 +4,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     desc = 'LSP actions',
     callback = function(event)
         local opts = {buffer = event.buf}
-        local wk = require("which-key")
 
         -- Note: commented out keymaps are defined by default in nvim
 
@@ -29,22 +28,26 @@ vim.api.nvim_create_autocmd('LspAttach', {
                        function() vim.lsp.buf.format({async = true}) end, opts)
         vim.keymap.set('n', '<F4>', function() vim.lsp.buf.code_action() end,
                        opts)
-        -- add help texts to which-key:
-        wk.add({
-            { 'K', desc = 'hover' },
-            { 'gr', group = '"goto" LSP' },
-            { 'grd', desc = 'goto definition' },
-            { 'grD', desc = 'goto declaration' },
-            { 'gri', desc = 'goto implementation' },
-            { 'grt', desc = 'goto type definition' },
-            { 'grr', desc = 'goto references' },
-            { 'gra', desc = 'code action' },
-            { 'grs', desc = 'signature help' },
-            { 'grn', desc = 'rename symbol' },
-            { '<F3>', desc = 'format buffer' },
-            { '<F4>', desc = 'code action' },
-        })
     end,
+})
+
+-- add help texts to which-key:
+-- *don't* put this inside the autocmd above, otherwise it will execute for
+-- every lsp attaching to a buffer and create :checkhealth issues:
+local wk = require("which-key")
+wk.add({
+    { 'K',    desc = 'hover' },
+    { 'gr',   group = '"goto" LSP' },
+    { 'grd',  desc = 'goto definition' },
+    { 'grD',  desc = 'goto declaration' },
+    { 'gri',  desc = 'goto implementation' },
+    { 'grt',  desc = 'goto type definition' },
+    { 'grr',  desc = 'goto references' },
+    { 'gra',  desc = 'code action' },
+    { 'grs',  desc = 'signature help' },
+    { 'grn',  desc = 'rename symbol' },
+    { '<F3>', desc = 'format buffer' },
+    { '<F4>', desc = 'code action' },
 })
 
 -- setup lsp-signature:
