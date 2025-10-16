bf3b65c31e358a1f5d213b3c98e43c4d1f81b839
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2025-10-16 22:45:54 +0200

    nvim: fix deprecated vim.fn.sign_define

--- .config/nvim/lua/custom/diagnostics.lua
+++ .config/nvim/lua/custom/diagnostics.lua
@@ -1,8 +1,17 @@
-vim.diagnostic.config({ virtual_text = { current_line = true }})
-
--- Change gutter symbols:
-local signs = { Error = "✘ ", Warn = "▲ ", Hint = "⚑ ", Info = "» " }
+-- gutter symbols:
+local signs = { ERROR = "✘ ", WARN = "▲ ", HINT = "⚑ ", INFO = "» " }
+local text = { }
+local texthl = { }
+local numhl = { }
 for type, icon in pairs(signs) do
     local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    -- vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+    text[vim.diagnostic.severity[type]] = icon
+    texthl[vim.diagnostic.severity[type]] = hl
+    numhl[vim.diagnostic.severity[type]] = hl
 end
+
+vim.diagnostic.config({
+    virtual_text = { current_line = true },
+    signs = { text = text, texthl = texthl, numhl = numhl }
+})
