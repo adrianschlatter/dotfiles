3a5ee45cff80e2089f27319f41a2c2de3f7835e0
Adrian Schlatter <10478149+adrianschlatter@users.noreply.github.com>
2025-12-21 12:54:41 +0100

    nvim: telescope find man pages <leader>fm

--- .config/nvim/after/plugin/telescope.lua
+++ .config/nvim/after/plugin/telescope.lua
@@ -8,7 +8,8 @@ vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
 vim.keymap.set('n', '<leader>fd', builtin.diagnostics, {})
-vim.keymap.set('n', '<leader>do', vim.diagnostic.open_float, {})
+vim.keymap.set('n', '<leader>fm', builtin.man_pages, {})
+vim.keymap.set('n', '<leader>fo', vim.diagnostic.open_float, {})
 
 wk.add({
     { '<leader>f', group = 'find' },
@@ -18,5 +19,6 @@ wk.add({
     { '<leader>fb', desc = 'buffers' },
     { '<leader>fh', desc = 'help tags' },
     { '<leader>fd', desc = 'diagnostics' },
+    { '<leader>fm', desc = 'man pages' },
     { '<leader>fo', desc = 'diagnostic, open float' },
 })
